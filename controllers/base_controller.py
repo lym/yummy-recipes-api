12a8d3b287864342813fcd5034bb1145eb4a895a
Salym Senyonga <salymsash@gmail.com>
2017-10-25 13:27:58 +0300

    Add utility methods to BaseController
    - BaseController.authorized_and_owns_recipe(): Checks that the user
    attached to a request is both authorized to access protected resources
    and owns the recipe they are referencing.
    
    - BaseController.auth_user(): Returns a user instance deduced from the
    incoming request.
    
    - Make BaseController.get_auth_token() a class method. This is more
    suitable for it.

--- controllers/base_controller.py
+++ controllers/base_controller.py
@@ -1,4 +1,7 @@
-from models import User
+from models import (
+    User,
+    Recipe,
+)
 
 
 class BaseController:
@@ -22,9 +25,32 @@ class BaseController:
             return False
         return True
 
-    def get_auth_token(req):
+    @classmethod
+    def get_auth_token(cls, req):
         auth_value = req.headers.get('Authorization')
         if auth_value is None:
             return None
         auth_token = auth_value.split(sep=' ')[-1]
         return auth_token
+
+    @classmethod
+    def authorized_and_owns_recipe(cls, req, recipe_id):
+        req_token = cls.get_auth_token(req)
+        if req_token is None:
+            return False
+        recipe = Recipe.query.filter_by(id=recipe_id).first()
+        recipe_owner_id = recipe.user_id
+        auth_user_token = req_token
+        auth_user = User.query.filter_by(auth_token=auth_user_token).first()
+        if recipe_owner_id == auth_user.id:
+            return True
+        return False
+
+    @classmethod
+    def auth_user(cls, req):
+        auth_token = cls.get_auth_token(req)
+        if auth_token is None:
+            return None
+        auth_user_token = auth_token
+        auth_user = User.query.filter_by(auth_token=auth_user_token).first()
+        return auth_user
