2b377e16be96f7737953c8e7565c72a8e80d75e5
Salym Senyonga <salymsash@gmail.com>
2017-10-27 00:11:46 +0300

    Add ability to edit a recipe
    We enable recipe update via the PATCH HTTP method as we want our clients
    to be able to specify only the fields that they want to update in the
    the request.

--- controllers/recipes_show_controller.py
+++ controllers/recipes_show_controller.py
@@ -39,6 +39,51 @@ class RecipesShowController(MethodView):
         res = jsonify(record)
         return res
 
+    def patch(self, recipe_id):
+        """ Update/Edit user data """
+        if not BaseController.authorized(request):
+            abort(401, 'Please supply valid user credentials')
+        recipe_id = int(recipe_id)
+        recipe = Recipe.query.filter_by(id=recipe_id).first()
+        if recipe_id is None:
+            abort(404, 'Requested recipe does not exist on the server')
+
+        # Ensure a user is updating their own recipe record
+        curr_user = BaseController.auth_user(request)
+        if recipe.user.id != curr_user.id:
+            abort(403, 'A user can only update their recipe records!')
+
+        user_id = request.get_json().get('user_id')
+        title = request.get_json().get('title')
+        description = request.get_json().get('description')
+
+        if user_id is not None:
+            recipe.user_id = user_id
+            DB.session.commit()
+        if title is not None:
+            recipe.title = title
+            DB.session.commit()
+        if description is not None:
+            recipe.description = description
+            DB.session.commit()
+        recipe = Recipe.query.filter_by(id=recipe_id).first()
+        record = {
+            'id': recipe.id,
+            'title': recipe.title,
+            'description': recipe.description,
+            'fulfilled': recipe.fulfilled,
+            'user_id': recipe.user.id,
+            'user': recipe.user.username,
+            'created': recipe.created,
+            'modified': recipe.modified,
+            'links': {
+                'self': RecipesEndpoint + str(recipe_id) + '/'
+
+            }
+        }
+        res = jsonify(record)
+        return res
+
     def delete(self, recipe_id):
         """ Delete a recipe and all its instructions and ingredients """
         if not BaseController.authorized(request):
