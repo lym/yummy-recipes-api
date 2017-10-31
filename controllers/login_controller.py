4e3cd98fde15f19eee5b1f97e947c902fc9f9c37
Salym Senyonga <salymsash@gmail.com>
2017-10-31 20:48:09 +0300

    Add error checks to LoginController
    The previous implementation of LoginController was throwing mean
    uncaught exceptions in cases where the request did not include the
    proper user data JSON payload. This commit accounts for those cases.

--- controllers/login_controller.py
+++ controllers/login_controller.py
@@ -10,8 +10,14 @@ from models import User
 
 class LoginController(MethodView):
     def post(self):
+        if request.get_json() is None:
+            abort(400, 'Please supply email and password')
         email = request.get_json().get('email')
+        if email is None:
+            abort(400, 'You need to supply an email address')
         passw = request.get_json().get('password')
+        if passw is None:
+            abort(400, 'You need to supply a password')
         existant_user = User.query.filter_by(email=email).first()
         if existant_user is None:
             abort(401)
