00490d301e9757db816abc7ea66d6aea24e3158f
Salym Senyonga <salymsash@gmail.com>
2017-11-06 23:06:40 +0300

    Add test for user search feature

--- test/controllers/test_users_controller.py
+++ test/controllers/test_users_controller.py
@@ -83,6 +83,24 @@ def test_user_list_pagination():
     assert req.json().__len__() == limit
 
 
+def test_user_search():
+    """ It should return users matching a given search criteria """
+    search_term = "fin"
+    url = 'http://127.0.0.1:5000/search/users?q={}'.format(search_term)
+    auth_headers = prepare_auth_headers()
+    req = requests.get(url, headers=auth_headers)
+    assert req.status_code == 200
+    users = req.json()
+    assert len(users) > 0  # Because we know a user matching this exists
+
+    search_term2 = "zqd"
+    url2 = 'http://127.0.0.1:5000/search/users?q={}'.format(search_term2)
+    req = requests.get(url2, headers=auth_headers)
+    assert req.status_code == 200
+    users2 = req.json()
+    assert len(users2) == 0  # Because we know a user matching this exists
+
+
 def test_user_deletion():
     """ It should delete a user and all their recipes, instructions and
     ingredients
