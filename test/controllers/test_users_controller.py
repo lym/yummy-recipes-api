a8c940003f1ff76f3a6473c90a4a377bbcc2f1df
Salym Senyonga <salymsash@gmail.com>
2017-11-06 21:50:56 +0300

    Refactor User controller/API test
    - Extract logic shared by different test cases into utility methods.
    - Hoist shared variables.

--- test/controllers/test_users_controller.py
+++ test/controllers/test_users_controller.py
@@ -5,6 +5,9 @@ a test the deletes all that was created at the end of the test suite as is the
 case currently.
 """
 
+user_list_url = 'http://127.0.0.1:5000/users/'
+
+
 def create_test_user():
     """ Create a test user, via an API endpoint """
     url = 'http://127.0.0.1:5000/users/'
@@ -19,18 +22,11 @@ def create_test_user():
     return req
 
 
-def test_user_registration():
-    """ It should create a new user """
-    req = create_test_user()
-    assert req.status_code == 200  # FIXME: This should be a 201
-
-
-def test_user_list_pagination():
-    """ It should return paginated list of users """
-    limit = 1
-    url = 'http://127.0.0.1:5000/users/?limit={}'.format(limit)
+def retrieve_auth_token():
+    """ It fetches a valid user authentication token, from the API server, to
+    be used in the subsequent requests for protected resources.
+    """
     login_url     = 'http://127.0.0.1:5000/login/'
-
     user_credentials = {
         "email"     : "smarat@atptour.com",
         "password"  : "testpass"
@@ -38,9 +34,49 @@ def test_user_list_pagination():
     auth_token = requests.post(
         login_url, json=user_credentials
     ).json().get('token')
+    return auth_token
+
+
+def prepare_auth_headers():
+    """ Creates the authentication headers dictionary for use with requests for
+    protected resources.
+    """
+    auth_token = retrieve_auth_token()
     auth_headers = {
         'Authorization': 'Token {}'.format(auth_token)
     }
+    return auth_headers
+
+
+def test_user_registration():
+    """ It should create a new user """
+    req = create_test_user()
+    assert req.status_code == 200  # FIXME: This should be a 201
+
+
+def test_user_attributes():
+    """ Check that expected user attributes are returned """
+    user_attributes = (
+        "first_name",
+        "last_name",
+        "username",
+        "email",
+        "id",
+        "email",
+        "links",
+    )
+    fetched_user = requests.get(
+        user_list_url, headers=prepare_auth_headers()
+    ).json()[0]
+    for attr in user_attributes:
+        assert attr in fetched_user.keys()
+
+
+def test_user_list_pagination():
+    """ It should return paginated list of users """
+    limit = 1
+    url = 'http://127.0.0.1:5000/users/?limit={}'.format(limit)
+    auth_headers = prepare_auth_headers()
     req = requests.get(url, headers=auth_headers)
 
     assert req.status_code == 200
@@ -52,17 +88,7 @@ def test_user_deletion():
     ingredients
     """
     user_list_url = 'http://127.0.0.1:5000/users/'
-    login_url     = 'http://127.0.0.1:5000/login/'
-    user_credentials = {
-        "email"     : "smarat@atptour.com",
-        "password"  : "testpass"
-    }
-    auth_token = requests.post(
-        login_url, json=user_credentials
-    ).json().get('token')
-    auth_headers = {
-        'Authorization': 'Token {}'.format(auth_token)
-    }
+    auth_headers = prepare_auth_headers()
     users = requests.get(user_list_url, headers=auth_headers)
 
     user_id = users.json()[-1].get('id')  # Last User's ID
